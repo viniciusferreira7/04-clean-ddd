083b353abe3ba50679a628d40773c6e1c10e394e
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-12-24 14:16:26 -0300

    feat: creates an entity class to be used for extensions to another class

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entity'
 import type { Slug } from './value-object/slug'
 
 interface QuestionProps {
@@ -8,18 +8,5 @@ interface QuestionProps {
   authorId: string,
 }
 
-export class Question {
-  public title: string
-  public slug: Slug
-  public content: string
-  public id: string
-  public authorId: string
-
-  constructor(props: QuestionProps, id?: string){
-    this.title = props.title
-    this.slug = props.slug
-    this.content = props.content
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
+export class Question extends Entity<QuestionProps> {
 }
