f07269a0191cc5586556e3bb0ceffcbddd037023
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-04-11 10:23:39 -0300

    feat: add the domain event structure

--- src/domain/forum/enterprise/entities/question-attachment-list.ts
+++ src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -4,6 +4,6 @@ import type { QuestionAttachment } from './question-attachment'
 
 export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
   compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
-    return a.attachmentId === b.attachmentId
+    return a.attachmentId.equals(b.attachmentId)
   }
 }
